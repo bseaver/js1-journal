a9ab39946e9259aa9046bd7812e85c51cc59abb7
Benjamin T. Seaver and Charles Peden <benjamin@>
2017-03-14 11:56:44 -0700

    promise

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,11 +5,27 @@ $(document).ready(function() {
     var city = $('#location').val();
     $('#location').val("");
     $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(JSON.stringify(response));
-      console.log(response);
-       $('.showWeather').text("The weather in " + city + " is " + response.weather[0].description);
-    });
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+      .then
+      (
+        function(response)
+        {
+          console.log(JSON.stringify(response));
+          console.log(response);
+           $('.showWeather').text("The weather in " + city + " is " + response.weather[0].description);
+         }
+      )
+      .fail
+      (
+        function(error)
+        {
+          console.log(JSON.stringify(error));
+          console.log(error);
+
+          $('.showWeather').text(error.responseJSON.message);
+        }
+      );
+
     console.log("Notice: The GET request has been made.");
 
   });
